dbd3ef27a611c40282ff8d21948afda18560d60b
Henry Letton <50699087+henryletton@users.noreply.github.com>
2022-06-01 14:34:26 +0100

    Another change

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -8,6 +8,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello there! I am running.";
+        return "Hey there! I am running.";
     }
 }
